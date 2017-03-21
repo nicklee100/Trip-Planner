f060a0355758a207f64db8be88baa54f9cdf0a39
gklapman <gabiklapman@gmail.com>
2017-03-21 08:19:20 -0500

    app and layout

--- static-ui-start/app.js
+++ static-ui-start/app.js
@@ -5,18 +5,18 @@ const chalk = require('chalk');
 const nunjucks = require('nunjucks');
 
 const {
-  db,
-  Place,
-  Hotel,
-  Restaurant,
-  Activity,
+    db,
+    Place,
+    Hotel,
+    Restaurant,
+    Activity,
 } = require('./db')
 
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 nunjucks.configure('views', { noCache: true });
@@ -25,25 +25,29 @@ app.set('view engine', 'html');
 
 app.use(express.static('public'));
 
-app.use(express.static('./bower_components'));
+app.use(express.static('./node_modules'));
+
+app.get('/theteam', function(req, res, next) {
+    res.render('theteam');
+})
 
 // root path
 app.get('/', function(req, res, next) {
 
-    const placePending =  Place.findAll();
+    const placePending = Place.findAll();
     const hotelPending = Hotel.findAll();
     const restaurantPending = Restaurant.findAll();
     const activityPending = Activity.findAll()
 
-    Promise.all([placePending,hotelPending,restaurantPending,activityPending])
-        .then( values => {
+    Promise.all([placePending, hotelPending, restaurantPending, activityPending])
+        .then(values => {
             //console.log(values[0])
             let places = values[0];
             let hotels = values[1];
             let restaurants = values[2];
             let activities = values[3];
             console.log(restaurants)
-            res.render('index',{hotels, places, restaurants, activities})
+            res.render('index', { hotels, places, restaurants, activities })
         }, error => {
             //res.render('index',)
             console.log('error! ', error)
@@ -52,17 +56,17 @@ app.get('/', function(req, res, next) {
 
 // error handling
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500)
-    .render('error', {message: 'oh no :-('});
+    res.status(err.status || 500)
+        .render('error', { message: 'oh no :-(' });
 });
 
 db.sync()
-  .then(() => {
-    console.log(chalk.cyan('DB SYNCED!'));
-    app.listen(3000, () => {
-      console.log(chalk.green('Planning on port 3000'));
+    .then(() => {
+        console.log(chalk.cyan('DB SYNCED!'));
+        app.listen(3000, () => {
+            console.log(chalk.green('Planning on port 3000'));
+        });
+    })
+    .catch(err => {
+        console.log(chalk.red('DB SYNC WENT WRONG...'));
     });
-  })
-  .catch(err => {
-    console.log(chalk.red('DB SYNC WENT WRONG...'));
-  });
